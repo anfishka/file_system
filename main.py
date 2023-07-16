1814000993441a78b2648952f59490a5d633e13b
anfishka <97487005+anfishka@users.noreply.github.com>
2023-07-17 01:08:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 # 3) Считать файл чисел и вывести их сумму
 # 4) Создать приложение телефонной книги с хранением фио контакта и множества его номеров телефонаt_hi(name):
 
-def createFile():
+
+import random
+
+def nameFile():
     invalid_charachters = ['/', '\\', '*', '<', '>', '|', '?', ':', '"', "'", '!', '%', " "]
 
     while True:
@@ -26,7 +29,49 @@ def createFile():
         else:
             print("Invalid name! Please try again")
 
+def randNums(amount):
+    nums = []
+    for i in range(amount):
+        num = random.randint(1,100)
+        nums.append(num)
+    return nums
+
+def sumNums(nums):
+    splitted_str = nums.split(",")
+    sum = 0
+    for i in splitted_str:
+        sum += int(i)
+    return str(sum)
+
+def createFile():
+    if __name__ == '__main__':
+        txt_file = nameFile() + '.txt'
+        file = open(txt_file, 'a')
+        file.close()
+        print(f"File has been successfully created with name : {txt_file}")
+
+def createFileWithNums():
+    if __name__ == '__main__':
+        txt_file = nameFile() + '.txt'
+        file = open(txt_file, 'w')
+        nums = ",".join(map(str, randNums(10)))
+        file.write(nums)
+        file.close()
+        print(f"File has been successfully created with the name {txt_file} and it contains : {nums}")
+
+def readFileAndSumNums(name_of_file):
+    if __name__ == '__main__':
+        file = open(name_of_file, 'r')
+        content = file.read()
+        result = sumNums(content)
+        file.close()
+        print(f"Sum of {content} is {result}")
 
 
-if __name__ == '__main__':
-    txt_file = createFile() + '.txt'
+#
+# createFile()
+# createFileWithNums()
+readFileAndSumNums("nnnm.txt")
+
+
+
